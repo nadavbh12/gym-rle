8860fc77052b342dbf7bbeeb0560bffe6da939df
nadavbh12 <nadavbh@gmail.com>
2017-03-22 16:25:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='gym_rle',
       version='0.0.1',
       install_requires=['gym>=0.2.3',
-                        'rle_python_interface>=1.1.4', 'PyOpenGL']
+                        'rle_python_interface>=1.1.5', 'PyOpenGL']
 )
